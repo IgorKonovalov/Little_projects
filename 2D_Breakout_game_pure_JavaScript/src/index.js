46c5b0c9dc4df7038ce169bc12e5988f59132015
IgorKonovalov <konovalov.avp@gmail.com>
2016-12-18 21:03:28 +0300

    продолжение игры - paddle and bricks

--- 2D_Breakout_game_pure_JavaScript/src/index.js
+++ 2D_Breakout_game_pure_JavaScript/src/index.js
@@ -3,24 +3,166 @@ let ctx = canvas.getContext("2d");
 
 let x = canvas.width/2;
 let y = canvas.height-30;
+let dx = 2;
+let dy = -2;
 
-const dx = 2;
-const dy = -2;
+// bricks
 
+const brickRowCount = 3;
+const brickColumnCount = 5;
+const brickWidth = 75;
+const brickHeight = 20;
+const brickPadding = 10;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
 
-function drawBall() {
+let bricks = [];
+for(c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for( r = 0; r < brickRowCount; r++) {
+      bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+
+// ball and paddle
+
+const ballRadius = 10;
+const paddleHeight = 10;
+const paddleWidth = 75;
+let rightPressed = false;
+let leftPressed = false;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+// colors
+let red = 0;
+let green = 0;
+let blue = 255;
+let color = "rgb(" + red + ", " + green + ", " + blue + ")";
+
+// score
+
+let score = 0;
+
+function drawScore() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Score: " + score, 8, 20);
+}
+
+function getRandomColor() {
+  let max = 254;
+  red = Math.floor(Math.random() * max);
+  green = Math.floor(Math.random() * max);
+  blue = Math.floor(Math.random() * max);
+  color = "rgb(" + red + ", " + green + ", " + blue + ")";
+  return color;
+}
+
+function drawBricks() {
+  for(c=0; c < brickColumnCount; c++) {
+    for(r=0; r < brickRowCount; r++) {
+      if (bricks[c][r].status == 1) {
+        let brickX = ( c * (brickWidth + brickPadding)) + brickOffsetLeft;
+        let brickY = ( r * (brickHeight + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+}
+
+function collisionDetection() {
+  for(c=0; c < brickColumnCount; c++) {
+    for(r=0; r < brickRowCount; r++) {
+      let b = bricks[c][r];
+      if(b.status == 1) {
+        if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+          dy = -dy;
+          b.status = 0;
+          getRandomColor();
+        }
+      }
+    }
+  }
+}
+
+function drawPaddle() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
 }
 
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.closePath();
+}
+
+let gameover = false;
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
+  drawBricks();
+  collisionDetection();
   x += dx;
   y += dy;
+  if (!gameover) {
+    if (rightPressed && paddleX < canvas.width-paddleWidth) {
+      paddleX += 7;
+    }
+    else if (leftPressed && paddleX > 0) {
+      paddleX -= 7;
+    }
+
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+      dx = -dx;
+
+    }
+    if(y + dy < ballRadius) {
+      dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+      if(x > paddleX && x < paddleX + paddleWidth) {
+        dy = -dy - 0.1;
+      }
+      else {
+        alert("GAME OVER");
+        gameover = true;
+        document.location.reload();
+      }
+    }
+  }
 }
+
+function keyDownHandler(e) {
+  if(e.keyCode == 39) {
+    rightPressed = true;
+  }
+  else if(e.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if(e.keyCode == 39) {
+    rightPressed = false;
+  }
+  else if(e.keyCode == 37) {
+    leftPressed = false;
+  }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
 setInterval(draw, 10);
