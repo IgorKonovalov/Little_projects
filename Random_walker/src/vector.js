0c2e32fdec583a2e2808875b78ba1392ceb7c5cd
Igor Konovalov <konovalov.avp@gmail.com>
2017-02-01 21:38:41 +0300

    random functions

--- Random_walker/src/vector.js
+++ Random_walker/src/vector.js
@@ -18,6 +18,22 @@ Vector.prototype.multiply = function(factor) {
   return new Vector(x, y)
 }
 
+function randomNumber(min, max) {
+  if (min > 0)
+    return (max - min) * Math.random();
+  else
+    return (max - min) * Math.random() + min;
+}
+
+function randomSign() {
+  let test = Math.ceil(Math.random() * 2);
+  if (test == 1)
+    return -1;
+  else
+    return 1;
+}
+
+console.log(randomSign());
 let vector = new Vector(1,2);
 
 console.log(vector.multiply(10));
