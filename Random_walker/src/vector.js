76cfc88d3cfb932699f4dce9a47e9b8b3b06a11a
Igor Konovalov <konovalov.avp@gmail.com>
2017-02-01 21:06:59 +0300

    vector methods

--- Random_walker/src/vector.js
+++ Random_walker/src/vector.js
@@ -1,1 +1,23 @@
-console.log('hello');
+const canvas = document.querySelector("canvas");
+const cx = canvas.getContext("2d");
+
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Vector.prototype.plus = function(other) {
+  x = this.x + other.x;
+  y = this.y + other.y;
+  return new Vector(x, y);
+}
+
+Vector.prototype.multiply = function(factor) {
+  x = this.x * factor;
+  y = this.y * factor;
+  return new Vector(x, y)
+}
+
+let vector = new Vector(1,2);
+
+console.log(vector.multiply(10));
