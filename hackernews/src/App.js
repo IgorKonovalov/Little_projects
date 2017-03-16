ff5cf9435f68dad5cfb408aa6c67dc756d576002
Igor Konovalov <konovalov.avp@gmail.com>
2017-03-16 20:04:27 +0300

    react experiments initial

---
+++ hackernews/src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const list = [
+  {
+    title: 'React',
+    url: 'http://igorkonovalov.github.io',
+  }]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello there</h1>
+      </div>
+    )
+  }
+}
+
+
+export default App
