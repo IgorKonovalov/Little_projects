aacf6719fe52b5c25876bd193e875140829e088d
Igor Konovalov <konovalov.avp@gmail.com>
2017-03-24 16:26:59 +0300

    fixed bug with even leaves, minor tweaks

--- Maurer_Rose_Walker/index.js
+++ Maurer_Rose_Walker/index.js
@@ -1,24 +1,58 @@
 const canvas = document.getElementById('canvas')
 const cx = canvas.getContext('2d')
+const inputN = document.getElementById('n')
+const startB = document.getElementById('start')
 
 const xStart = canvas.width/2
 const yStart = canvas.height/2
 
-const n = 3
-const d = 1
-const k = n/d
+let n, i, pathCounter, drawing, started, indexP
+let points, path = []
 
-let points = []
+startB.addEventListener('click', () => {
+  cx.clearRect(0, 0, canvas.width, canvas.height)
+  if (!started) {
+    startB.innerHTML = 'STOP'
+    points = []
+    i = 0
+    pathCounter = 0
+    path = []
+    n = inputN.value
+    getPoints(n)
+    started = true
+    drawing = setInterval(() => {
+      drawRose(n)
+      drawPath(i)
+      i++
+      if (n % 2 == 1) {
+        if (i === 361) {
+          i = 0
+        }
+      } else {
+        if (i === 720) {
+          i = 0
+        }
+      }
 
-drawRose = k => {
+    }, 30)
+  } else {
+    startB.innerHTML = 'START'
+    started = false
+    console.log('cleared')
+    clearInterval(drawing)
+  }
+})
+
+
+drawRose = n => {
   cx.clearRect(0, 0, canvas.width, canvas.height)
   cx.beginPath()
   cx.strokeStyle = 'white'
   cx.lineCap = 'round'
   cx.lineWidth = .3
-  for (let a = 0; a < 360 * Math.ceil(d); a += 1) {
+  for (let a = 0; a < 360; a += 1) {
     deg = a * Math.PI / 180
-    r = Math.sin(- k * deg) * (xStart - 20)
+    r = Math.sin(n * deg) * (xStart - 20)
     x = xStart + r * Math.cos(deg)
     y = xStart + r * Math.sin(deg)
     cx.lineTo(x, y)
@@ -26,32 +60,17 @@ drawRose = k => {
   cx.stroke()
 }
 
-getPoints = k => {
-  for (let a = 0; a < 360 * Math.ceil(d); a += .5) {
+getPoints = n => {
+  for (let a = 0; a < 360; a += .5) {
     deg = a * Math.PI / 180
-    r = Math.sin(- k * deg) * (xStart - 20)
+    r = Math.sin(n * deg) * (xStart - 20)
     x = xStart + r * Math.cos(deg)
     y = xStart + r * Math.sin(deg)
     points.push({x: x, y: y})
   }
 }
 
-drawRose(k)
-getPoints(k)
-
-
-let i = 0
-let pathCounter = 0
-let path = []
-
-drawPoint = (point, index) => {
-  cx.beginPath()
-  cx.fillRect(point.x, point.y, 10, 10)
-  cx.fillStyle = `hsl(${index}, 70%, 50%)`
-  cx.fill()
-}
-
-drawPath = (index) => {
+drawPath = index => {
   if (pathCounter < 200) {
     path.push(index)
   } else {
@@ -60,12 +79,12 @@ drawPath = (index) => {
   }
   for (let j = 0; j < pathCounter - 1; j++) {
     cx.beginPath()
-    let indexP = path[j]
+    indexP = path[j]
     if (!indexP) {
       indexP = index
     }
-    cx.strokeStyle = `hsl(${indexP}, 70%, 50%)`
-    cx.lineWidth = 10
+    cx.strokeStyle = `hsl(${indexP}, 100%, 50%)`
+    cx.lineWidth = 15
     if (indexP >= 1) {
       cx.moveTo(points[indexP - 1].x, points[indexP - 1].y)
     }
@@ -74,13 +93,3 @@ drawPath = (index) => {
   }
   pathCounter++
 }
-
-setInterval(() => {
-  drawRose(k)
-  // drawPoint(points[i], i)
-  drawPath(i)
-  i++
-  if (i === 361 * d) {
-    i = 0
-  }
-}, 30)
