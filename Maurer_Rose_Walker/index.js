05e9e0887f4ff28854960bc07a772b2eb4c0854e
Igor Konovalov <konovalov.avp@gmail.com>
2017-03-24 13:22:40 +0300

    one bug left tll secess

--- Maurer_Rose_Walker/index.js
+++ Maurer_Rose_Walker/index.js
@@ -4,8 +4,8 @@ const cx = canvas.getContext('2d')
 const xStart = canvas.width/2
 const yStart = canvas.height/2
 
-let n = 3
-let d = 1
+let n = 7
+let d = 4
 let k = n/d
 
 let points = []
@@ -16,22 +16,33 @@ drawRose = k => {
   cx.strokeStyle = 'white'
   cx.lineCap = 'round'
   cx.lineWidth = .3
-  for (let a = 0; a < 360 * Math.ceil(d); a += .0625) {
+  for (let a = 0; a < 360 * Math.ceil(d); a += 1) {
     deg = a * Math.PI / 180
     r = Math.sin(- k * deg) * (xStart - 20)
     x = xStart + r * Math.cos(deg)
     y = xStart + r * Math.sin(deg)
-    points.push({x: x, y: y})
     cx.lineTo(x, y)
   }
   cx.stroke()
 }
 
+getPoints = k => {
+  for (let a = 0; a < 360 * Math.ceil(d); a += .125) {
+    deg = a * Math.PI / 180
+    r = Math.sin(- k * deg) * (xStart - 20)
+    x = xStart + r * Math.cos(deg)
+    y = xStart + r * Math.sin(deg)
+    points.push({x: x, y: y})
+  }
+}
+
 drawRose(k)
+getPoints(k)
 
 
 let i = 0
-let prev, next;
+let pathCounter = 0
+let path = []
 
 drawPoint = (point, index) => {
   let prev = point
@@ -39,13 +50,32 @@ drawPoint = (point, index) => {
   cx.fillRect(point.x, point.y, 10, 10)
   cx.fillStyle = `hsl(${index}, 70%, 50%)`
   cx.fill()
-  i++
+}
+
+drawPath = (index) => {
+  if (pathCounter < 1000) {
+    path.push(index)
+  } else {
+    path.shift()
+    path.push(index)
+  }
+  for (let i = 0; i < pathCounter; i++) {
+    let index = path[i]
+    cx.beginPath()
+    cx.strokeStyle = `hsl(${index}, 70%, 50%)`
+    cx.lineWidth = 10
+    cx.lineTo(points[index].x, points[index].y)
+    cx.stroke()
+  }
+  pathCounter++
 }
 
 setInterval(() => {
-  drawPoint(points[i], i)
+  drawRose(k)
+  // drawPoint(points[i], i)
+  drawPath(i)
   i++
-  if (i === 5758) {
+  if (i === 2878 * d) {
     i = 0
   }
 }, 10)
