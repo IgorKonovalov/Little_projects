bacd9e04d034580c54b6df9b82584419b0091d69
Igor Konovalov <konovalov.avp@gmail.com>
2017-03-26 17:16:44 +0300

    soundcloud client initial

---
+++ Soundcloud_client_React_Redux/src/components/Stream.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Stream({tracks = []}) {
+  return (
+    <div>
+      {
+        tracks.map((track, key) => {
+          return <div className="track" key={key}>{track.title}</div>
+        })
+      }
+    </div>
+  )
+}
+export default Stream
