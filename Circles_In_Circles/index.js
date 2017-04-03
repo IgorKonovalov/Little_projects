7ce493de9a5d12133cb3255898dfaf7541bb23ce
Igor Konovalov <konovalov.avp@gmail.com>
2017-04-03 10:29:16 +0300

    points on circle func

--- Circles_In_Circles/index.js
+++ Circles_In_Circles/index.js
@@ -1,12 +1,32 @@
 const canvas = document.getElementById('canvas')
 const cx = canvas.getContext('2d')
 
+cx.lineWidth = 2
+
+function drawCircle(centerX, centerY, radius) {
+  cx.beginPath()
+  cx.strokeStyle = 'white'
+  cx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+  cx.stroke()
+}
+
+function getPointsByCirle(radiusBig, radiusSmall, centerX, centerY) {
+  const radius = radiusBig - radiusSmall
+  let pointsArray = []
+  let x, y
+  for (let i = 0; i < 360; i++) {
+    let degreeRad = i  * Math.PI / 180
+    x = centerX + radius * Math.cos(degreeRad)
+    y = centerX + radius * Math.sin(degreeRad)
+    pointsArray.push({x: x, y: y})
+  }
+  console.log(pointsArray);
+}
+
 const centerX = canvas.width/2
 const centerY = canvas.height/2
 const radius = 320
 
-cx.beginPath()
-cx.strokeStyle = 'white'
-cx.lineWidth = 2
-cx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-cx.stroke()
+drawCircle(centerX, centerY, radius)
+
+getPointsByCirle(radius, 30, centerX, centerY)
