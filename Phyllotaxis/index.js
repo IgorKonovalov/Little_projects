1233a2d140d9c1f14c3ff2bdddd1447fa1d9e9ed
Igor Konovalov <konovalov.avp@gmail.com>
2017-04-08 11:46:13 +0300

    angle input, finish

--- Phyllotaxis/index.js
+++ Phyllotaxis/index.js
@@ -1,36 +1,52 @@
 const canvas = document.getElementById('canvas')
 const cx = canvas.getContext('2d')
+const button = document.getElementById('start')
+const angleInput = document.getElementById('angle')
 
 function degreeToRad(degree) {
   const rad = degree / 180 * Math.PI
   return rad
 }
 
-function reMap(value, start1, stop1, start2, stop2) {
-  const slope = (stop2 - start2) / (stop1 - start1)
-  return start2 + slope * (value - start1)
-}
-
 const centerX = canvas.width / 2
 const centerY = canvas.height / 2
 
+let drawingInterval
 let n = 0
 let c = 10
+let running = false
+let angle = Number(angleInput.value)
 
 cx.fillStyle = 'white'
 
-function draw() {
-  let angle = degreeToRad(n * 135.5);
+function draw(angle) {
+  let a = degreeToRad(n * angle);
   let rad = c * Math.sqrt(n);
-  let x = rad * Math.cos(angle) + centerX;
-  let y = rad * Math.sin(angle) + centerY;
+  let x = rad * Math.cos(a) + centerX;
+  let y = rad * Math.sin(a) + centerY;
   cx.beginPath();
   cx.arc(x, y, 5, 0, 2 * Math.PI);
-  cx.fillStyle = `hsl(${n}, 100%, 50%)`
+  cx.fillStyle = `hsl(${n * 0.6}, 100%, 50%)`
   cx.fill();
+  if (rad > canvas.width / 2 - 20) {
+    clearInterval(drawingInterval)
+  }
   n++;
 }
 
-setInterval(()=> {
-  draw()
-}, 10)
+button.addEventListener('click', () => {
+  if (running) {
+    clearInterval(drawingInterval)
+    button.innerHTML = 'START'
+    running = false
+  } else {
+    cx.clearRect(0, 0, canvas.width, canvas.height)
+    angle = angleInput.value
+    n = 0
+    drawingInterval = setInterval(()=> {
+      draw(angle)
+    }, 10)
+    button.innerHTML = 'STOP'
+    running = true
+  }
+})
