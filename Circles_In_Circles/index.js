4eb4df45d8fb92633bf5b03ee73870b564148ae2
Igor Konovalov <konovalov.avp@gmail.com>
2017-04-15 15:21:45 +0300

    circles done, needs adjastment

--- Circles_In_Circles/index.js
+++ Circles_In_Circles/index.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas')
 const cx = canvas.getContext('2d')
 
-cx.lineWidth = 2
+cx.lineWidth = 5
 
 function Circle(radius, radiusSmall, color) {
   this.radius = radius
@@ -26,9 +26,9 @@ function Circle(radius, radiusSmall, color) {
 }
 
 function draw(circle, centerX, centerY) {
+  cx.setTransform(1, 0, 0, 1, centerX, centerY);
   cx.beginPath();
   cx.strokeStyle = circle.color;
-  cx.translate(centerX, centerY);
   cx.arc(0, 0, circle.radius, 0, 2 * Math.PI, false);
   cx.stroke();
 }
@@ -39,19 +39,31 @@ const radiusBig = 320
 const radiusMiddle = 120
 const radiusSmall = 40
 
-let bigCircle = new Circle(320, 120, 'white')
-let middleCircle = new Circle(120, 40, 'white')
+const bigCircle = new Circle(320, 120, 'white')
+const middleCircle = new Circle(120, 40, 'white')
+const smallCircle = new Circle(40, 10, 'white')
+const smallerCircle = new Circle(10, 2, 'lightBlue')
 
-draw(bigCircle, centerX, centerY)
 
 const points = bigCircle.getPointsByCirle();
-let i = 0
+const middlePoints = middleCircle.getPointsByCirle();
+const smallPoints = smallCircle.getPointsByCirle();
 
-setInterval(() => {
-  draw(middleCircle, points[i].x, points[i].y)
-  i++
-  cx.setTransform(1,0,0,1,centerX,centerY);
-  if (i > 359) {
-    i = 0
-  }
+let i = 0
+let j = 359
+let z = 359
+
+const drawing = setInterval(() => {
+  cx.setTransform(1, 0, 0, 1, 0, 0);
+  cx.clearRect(0, 0, canvas.width, canvas.height);
+  draw(bigCircle, centerX, centerY)
+  draw(middleCircle, points[i].x + centerX, points[i].y + centerY)
+  draw(smallCircle, middlePoints[j].x + points[i].x + centerX, middlePoints[j].y + points[i].y + centerY)
+  draw(smallerCircle, smallPoints[i].x + middlePoints[j].x + points[i].x + centerX, smallPoints[i].y + middlePoints[j].y + points[i].y + centerY)
+  i ++;
+  j --;
+  z --;
+  if (i > 359) {i = 0}
+  if (j == 0) {j = 359}
+  if (z == 0) {z = 359}
 }, 10)
