b4d09b0a31056d3adaadf1a4ff01729de768cea3
IgorKonovalov <konovalov.avp@gmail.com>
2017-04-23 11:41:12 +0300

    velocity in circles

--- Circles_In_Circles/index.js
+++ Circles_In_Circles/index.js
@@ -1,28 +1,27 @@
-const canvas = document.getElementById('canvas')
-const cx = canvas.getContext('2d')
+const canvas = document.getElementById("canvas");
+const cx = canvas.getContext("2d");
 
-cx.lineWidth = 5
+cx.lineWidth = 2;
 
-function Circle(radius, radiusSmall, color) {
-  this.radius = radius
-  this.color = color
-  this.radiusSmall = radiusSmall
+function Circle(radius, radiusSmall, color, velocity = 1) {
+  this.radius = radius;
+  this.color = color;
+  this.radiusSmall = radiusSmall;
 
-  this.pointsArray = []
+  this.pointsArray = [];
 
   this.getPointsByCirle = function() {
-    const radiusInner = this.radius - this.radiusSmall
-    let pointsArray = []
-    let x, y
-    for (let i = 0; i < 360; i ++) {
-      let degreeRad = i  * Math.PI / 180
-      x = radiusInner * Math.cos(degreeRad)
-      y = radiusInner * Math.sin(degreeRad)
-      pointsArray.push({x, y})
+    const radiusInner = this.radius - this.radiusSmall;
+    let pointsArray = [];
+    let x, y;
+    for (let i = 0; i < 360; i++) {
+      let degreeRad = i * velocity * Math.PI / 180;
+      x = radiusInner * Math.cos(degreeRad);
+      y = radiusInner * Math.sin(degreeRad);
+      pointsArray.push({ x, y });
     }
-    return this.pointsArray = pointsArray;
-  }
-
+    return (this.pointsArray = pointsArray);
+  };
 }
 
 function draw(circle, centerX, centerY) {
@@ -33,37 +32,40 @@ function draw(circle, centerX, centerY) {
   cx.stroke();
 }
 
-const centerX = canvas.width/2
-const centerY = canvas.height/2
-const radiusBig = 320
-const radiusMiddle = 120
-const radiusSmall = 40
+const centerX = canvas.width / 2;
+const centerY = canvas.height / 2;
+const radiusBig = 320;
+const radiusMiddle = 120;
+const radiusSmall = 40;
 
-const bigCircle = new Circle(320, 120, 'white')
-const middleCircle = new Circle(120, 40, 'white')
-const smallCircle = new Circle(40, 10, 'white')
-const smallerCircle = new Circle(10, 2, 'lightBlue')
-
+const bigCircle = new Circle(320, 120, "white", 1);
+const middleCircle = new Circle(120, 40, "white", 2);
+const smallCircle = new Circle(40, 10, "white", 4);
+const smallerCircle = new Circle(10, 2, "lightBlue", 8);
 
 const points = bigCircle.getPointsByCirle();
 const middlePoints = middleCircle.getPointsByCirle();
 const smallPoints = smallCircle.getPointsByCirle();
 
-let i = 0
-let j = 359
-let z = 359
+let i = 0;
 
 const drawing = setInterval(() => {
   cx.setTransform(1, 0, 0, 1, 0, 0);
   cx.clearRect(0, 0, canvas.width, canvas.height);
-  draw(bigCircle, centerX, centerY)
-  draw(middleCircle, points[i].x + centerX, points[i].y + centerY)
-  draw(smallCircle, middlePoints[j].x + points[i].x + centerX, middlePoints[j].y + points[i].y + centerY)
-  draw(smallerCircle, smallPoints[i].x + middlePoints[j].x + points[i].x + centerX, smallPoints[i].y + middlePoints[j].y + points[i].y + centerY)
-  i ++;
-  j --;
-  z --;
-  if (i > 359) {i = 0}
-  if (j == 0) {j = 359}
-  if (z == 0) {z = 359}
-}, 10)
+  draw(bigCircle, centerX, centerY);
+  draw(middleCircle, points[i].x + centerX, points[i].y + centerY);
+  draw(
+    smallCircle,
+    middlePoints[i].x + points[i].x + centerX,
+    middlePoints[i].y + points[i].y + centerY
+  );
+  draw(
+    smallerCircle,
+    smallPoints[i].x + middlePoints[i].x + points[i].x + centerX,
+    smallPoints[i].y + middlePoints[i].y + points[i].y + centerY
+  );
+  i++;
+  if (i > 359) {
+    i = 0;
+  }
+}, 10);
